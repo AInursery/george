80a1e586bb660ca7a42666796532e297e3eec8a2
ngaymoi28 <ngaymoi28@gmail.com>
2014-11-15 17:59:10 +0100

    Add yml config file to deploy on Bluemix

--- app/app.py
+++ app/app.py
@@ -79,5 +79,8 @@ def api(q):
         'sentiment': query.sentiment.polarity
     })
 
+
+port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(host='0.0.0.0', port=int(port))
+    
