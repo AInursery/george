bf173813fe85acb54fbb2aa8acd0d707f947b587
Nabellaleen <Nabellaleen@users.noreply.github.com>
2014-11-15 17:57:24 +0100

    Make meme_query return a list

--- app/meme_query.py
+++ app/meme_query.py
@@ -1,28 +1,40 @@
 import requests
 import bs4 as BeautifulSoup
 
-API_URL = 'http://memegenerator.net/memes/search'
+BASE_URL = 'http://memegenerator.net'
+API_URL = '/memes/search'
 
 
 def meme_query(query):
+    return [meme for meme in meme_query_all(query)]
+
+
+def meme_query_all(query):
     params = {'q': query}
-    res = requests.get(API_URL, params=params)
+    res = requests.get('{0}{1}'.format(BASE_URL, API_URL), params=params)
     if res.status_code is 200:
         # Got an answer
         soup = BeautifulSoup.BeautifulSoup(res.content)
         meme_horizontal_gallery = soup.find('ul',
             attrs={'class': u'horizontal'})
-        first_meme = meme_horizontal_gallery.find('li')
-        if first_meme:
-            first_meme_img = first_meme.find('img')
-            first_meme_title = first_meme.find('a',
-                attrs={'class': u'name'})
-            return {'img': first_meme_img['src'],
-                    'title': first_meme_title['title']}
-        return
-    else:
-        # Error : TODO
-        return
+        memes = meme_horizontal_gallery.findAll('li')
+        for meme in memes:
+            result = {
+                'img': '',
+                'title': '',
+                'url': ''
+            }
+            meme_url = meme.find('a')
+            if meme_url:
+                result['url'] = '{0}{1}'.format(BASE_URL, meme_url['href'])
+            meme_img = meme_url.find('img')
+            if meme_img:
+                result['img'] = meme_img['src']
+            meme_title = meme.find('a',
+                attrs={'class': 'name'})
+            if meme_title:
+                result['title'] = meme_title['title']
+            yield result
 
 
 def main():
